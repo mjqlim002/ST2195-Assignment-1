f3639bc03eba5c2f2c56e552c34e31d7d75c8994
mjqlim002 <91530289+mjqlim002@users.noreply.github.com>
2021-12-09 17:12:16 +0800

    Add files via upload

---
+++ Assignment 1 (R studio).R
@@ -0,0 +1,21 @@
+#title:"Assignment 1 Part 2"
+#output: 
+  #html_document:
+    #df_print: paged
+#Printing Integers 1 to 10, w/o loop
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+
+#with loop
+for(i in c(1:10)) {
+  print(i)
+}
+
